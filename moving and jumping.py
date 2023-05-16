1cdaf6c96a5e03e66cccb3a823a604c270c7e5da
Keyyard <keyyard8888@gmail.com>
2023-05-16 23:15:34 +0700

    update

--- moving and jumping.py
+++ moving and jumping.py
@@ -11,8 +11,8 @@ keyyard = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Moving nonstop and comeback")
 
 # obstacle position
-obstacle_x = 0
-obstacle_y = 0
+obstacle_x = 700
+obstacle_y = 700
 obstacle_width = 0
 obstacle_height = 0
 # object current coordinates 
@@ -28,7 +28,12 @@ vel = 3
   
 # Indicates pygame is running
 run = True
-  
+
+#cloud x and y
+cloud_x = 480
+cloud2_x = 200
+cloud_y = 20
+cloud2_y = 40
 # Detects jump
 isJump = False
 jumpCount = 15
@@ -57,10 +62,10 @@ while run:
     keys = pygame.key.get_pressed()
 
     # if space bar is pressed  
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_SPACE] and y == 400:
         isJump = True
     
-    #move right every second
+    #move right every 100ms
     for i in range(100):
         if i%100 == 0:
             x += vel
@@ -74,35 +79,49 @@ while run:
          # if not jumping
     if not(isJump): 
         # if space bar is pressed
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and game_over == 0:
             isJump = True
     else:
         # if space bar is pressed
         if jumpCount >= -15: #count down the jump count until it reaches -20 (to get back the original position)
                 y -= (jumpCount * 2) * 0.5 # this is the formula for smooth jump: y = (x * abs(x)) * 0.5, x is the time of the jump
                 vel = 6 #increase speed for the jump
-                jumpCount -= 1 
+                jumpCount -= 1
+
         else: 
                 # making isJump equal to False when done, reseting the jumpCount
                 jumpCount = 15
                 isJump = False
                 vel = 3
-         
               
     # completely fill the surface object  
-    # with black colour  
-    keyyard.fill((0, 0, 0)) # this will also remove the previous rectangle
+    # with sky colour  
+    keyyard.fill((90,188,216)) # this will also remove the previous rectangle
     #fill the bottom part 450-500 with light green
-    pygame.draw.rect(keyyard, (160, 231, 125), (0, 420, 500, 500))
+    #ground 
+    ground = pygame.image.load('ground.png')
+    ground_rect = ground.get_rect()
+    #scale up the ground
+    ground = pygame.transform.scale(ground, (80, 80))
+    ground_rect.bottom = 440
+    #fill the bottom with ground
+    #ground cover screen
+    for i in range(0, 500, 80):
+        keyyard.blit(ground, (i, 420))
+
+    #pygame.draw.rect(keyyard, (160, 231, 125), (0, 420, 500, 500))
     # drawing player on screen which is rectangle here 
-    pygame.draw.rect(keyyard, (130, 182, 217), (x, y, width, height))
-    # obstacle
+    player = pygame.image.load('char.png')
+    player_rect = player.get_rect()
+    #scale up the player
+    player = pygame.transform.scale(player, (40, 40))
+    # obstacle 1
     if x < 5:
         #randomize the obstacle position number that % 50 = 0
         obstacle_x = random.randint(100, 460)
         if obstacle_x % 50 != 0:
             obstacle_x = random.randint(100, 460)
-        obstacle_y = 380
+        obstacle_y = 400
         obstacle_width = 40
         obstacle_height = 40
 
@@ -113,8 +132,29 @@ while run:
             y = 500
             #game over text
             game_over = 1
-    # draw obstacle
-    pygame.draw.rect(keyyard, (239, 134, 119), (obstacle_x, obstacle_y, obstacle_width, obstacle_height))
+    obstacle = pygame.image.load('spike.png')
+    obstacle_rect = obstacle.get_rect()
+    #scale up the obstacle 1 
+    obstacle = pygame.transform.scale(obstacle, (40, 40))
+    obstacle_rect.bottom = 400
+    keyyard.blit(obstacle, (obstacle_x, obstacle_y))
+
+    # clouds
+    cloud = pygame.image.load('cloud.png')
+    cloud_rect = cloud.get_rect()
+    #scale up the cloud
+    cloud = pygame.transform.scale(cloud, (180, 180))
+    cloud2 = pygame.transform.scale(cloud, (180, 180))
+    if cloud_x <= -180:
+        cloud_x = 480
+        cloud_y = random.randint(10, 40)
+    cloud_x = cloud_x - 1
+    cloud2_x = cloud2_x - 1
+    if cloud2_x <= -180:
+        cloud2_x = 480
+        cloud2_y = random.randint(10, 40)
+    keyyard.blit(cloud, (cloud_x, cloud_y))
+    keyyard.blit(cloud2, (cloud2_x, cloud2_y))
     #points
     font = pygame.font.SysFont("comicsansms", 20)
     text = font.render("Point: " + str(point), True, (255, 255, 255))
@@ -129,20 +169,21 @@ while run:
         text_subtitle_rect = text_subtitle.get_rect(center=(500/2, 500/2+50))
         keyyard.blit(text_title, (150, 200))
         keyyard.blit(text_subtitle, (150, 250))
-        obstacle_x = 0
-        obstacle_y = 0
+        obstacle_x = 700
+        obstacle_y = 700
         obstacle_width = 0
         obstacle_height = 0
         vel = 0
 
     # if key press = enter, reset the game
-    if keys[pygame.K_RETURN]:
+    if keys[pygame.K_RETURN] and game_over == 1:
         x = 4
         y = 400
         game_over = 0
         vel = 3
         point = 0
-
+    # render player
+    keyyard.blit(player, (x, y))
     # it refreshes the window
     pygame.display.update() 
 # closes the pygame window 
